3b99924f7f13cfdb9540083a98d62cddb13ae363
IttaloSantos <ittalosantoss@gmail.com>
2021-04-08 00:12:14 -0300

    Creating develop branch with additional admin login and password input

--- institutionSystem/platform.py
+++ institutionSystem/platform.py
@@ -2,5 +2,11 @@
 # -*- coding: utf-8 -*-
 
 class institutionPlatform(object):
-    def __init__(self):
-        pass
+    """
+    General plataform class where the institution can create its environment attributes
+    such as institution name, teacher and student register and so.
+    """
+
+    def __init__(self, login, password):
+        self.admLogin = login
+        self.admPass  = password
--- main.py
+++ main.py
@@ -2,9 +2,20 @@
 # -*- coding: utf-8 -*-
 
 from institutionSystem.platform import institutionPlatform
+from getpass import getpass
 
 def main():
-    tataCodeCollege = institutionPlatform()
+
+    print("################################################################"    )
+    print("#               Welcome to Tata Code College!                  #"    )
+    print("################################################################\n\n")
+
+    admLogin        = input("Input adm login: "     )
+    admPass         = getpass("Input adm password: ")
+
+    tataCodeCollege = institutionPlatform(admLogin, admPass)
+
+    print("\n\nDone!\n\n")
 
 if __name__ == '__main__':
     main()
